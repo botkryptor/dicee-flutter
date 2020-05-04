ed5830ba87b821157f6df6233bffe1da4d79b032
Animesh Gupta <guptanimesh1998@gmail.com>
2020-05-04 15:40:38 +0530

    My Dice App

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +16,49 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int left = 1;
+  int right = 2;
+  void change() {
+    left =
+        Random().nextInt(6) + 1; //nextint(max) generates // upto max-1 numbers
+    right = Random().nextInt(6) + 1;
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Center(
+      child: Row(
+        children: <Widget>[
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  change();
+                });
+              },
+              child: Image.asset('images/dice$left.png'),
+            ),
+            flex: 1,
+          ),
+          Expanded(
+            child: FlatButton(
+              onPressed: () {
+                setState(() {
+                  change();
+                });
+              },
+              child: Image.asset('images/dice$right.png'),
+            ),
+            flex: 1,
+          ),
+        ],
+      ),
+    );
   }
 }
